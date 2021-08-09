10179dea0aee9f6f164814f80ae09b5377abb2cc
Havva Yılmaz <86909580+thehavva@users.noreply.github.com>
2021-08-09 13:42:03 +0300

    Add files via upload

---
+++ DisplayingWindow/mainwindow.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <mydialog.h>
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setCentralWidget(ui->plainTextEdit);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_actionNew_Windows_triggered()
+{
+    MyDialog mDialog;
+    mDialog.setModal(true);
+    mDialog.exec();
+}
+
