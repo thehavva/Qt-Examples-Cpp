4507de1c3191eab56b589e9f6404db2765c656d3
Havva Yılmaz <86909580+thehavva@users.noreply.github.com>
2021-08-10 00:23:38 +0300

    Add files via upload

---
+++ QLineEdit/dialog.cpp
@@ -0,0 +1,22 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+#include <QMessageBox>
+Dialog::Dialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+
+void Dialog::on_pushButton_clicked()
+{
+    //ui->lineEdit->setText("hello world");
+    QMessageBox::information(this,"Title",ui->lineEdit->text());
+}
+
