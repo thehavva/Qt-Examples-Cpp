9f05bd00f9bd43b9a4e5af9ba3f1a73b78ba919c
Havva Yılmaz <86909580+thehavva@users.noreply.github.com>
2021-08-11 00:53:12 +0300

    Add files via upload

---
+++ QPushButton3/dialog.cpp
@@ -0,0 +1,24 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+
+#include <QtCore>
+#include <QtGui>
+#include <QMessageBox>
+Dialog::Dialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+
+void Dialog::on_pushButton_clicked()
+{
+    QMessageBox::information(this,"My Diolog","hello world!");
+}
+
