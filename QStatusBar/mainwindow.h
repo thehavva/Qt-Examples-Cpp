b2d666bf61bdf4b18f8dee6a349d59ba2697996f
Havva Yılmaz <86909580+thehavva@users.noreply.github.com>
2021-08-17 23:14:31 +0300

    Add files via upload

---
+++ QStatusBar/mainwindow.h
@@ -0,0 +1,29 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtCore>
+#include <QtGui>
+#include <QLabel>
+#include <QProgressBar>
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_actionDo_something_triggered();
+
+private:
+    Ui::MainWindow *ui;
+    QLabel *statlabel;
+    QProgressBar *statprogres;
+};
+#endif // MAINWINDOW_H
